b723130a1c5595eda36bcf25b6f1cd1bf3bdf552
Charles Chung <charles5j21z@gmail.com>
2020-04-22 12:31:28 -0400

    local

--- final.py
+++ final.py
@@ -165,7 +165,6 @@ def train_regressor(fingerprints, labels, train_set):
 	regr_multirf = MultiOutputRegressor(RandomForestRegressor(n_estimators=100,
 	                                                          max_depth=max_depth,
 	                                                          random_state=0))
-
 	
 	X = []
 	y = []
